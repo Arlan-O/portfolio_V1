b75e4c683c4a7ba45246dde3d7110c2ff151efd7
Arlan <arlan.oliveira022@gmail.com>
2026-05-17 02:53:34 -0300

    criação do mock de projetos

---
+++ data/projects.js
@@ -0,0 +1,43 @@
+const projects = [
+  {
+    id:"raizes-esg",
+    numero: "01",
+    categoria: "ESG | TAXTECH",
+    titulo: "RAÍZES ESG",
+    descricao:
+      "Plataforma conceitual desenvolvida a partir da observação dos impactos da reforma tributária e da dependência de benefícios fiscais por pequenas e médias empresas.",
+    reconhecimento: "3º Lugar em projeto acadêmico multidisciplinar.",
+    stacks: "Figma | Fluxo de sistema | Pesquisa de negócio",
+  },
+  {
+    id:"classificador-fiscal",
+    numero: "02",
+    categoria: "AUTOMAÇÃO | TAXTECH | PROCESSOS",
+    titulo: "CLASSIFICADOR FISCAL",
+    descricao:
+      "Estudo de automação aplicado à classificação tributária e estruturação de regras de negócio relacionadas ao cClassTrib e à reforma tributária.",
+    contexto: "Automação de processos | Regras tributárias",
+    exploracao: "n8n | Fluxos automatizados | Estruturação lógica ",
+    status: "Projeto em estudo e evolução contínua.",
+  },
+  {
+    id:"consulta-cnpj",
+    numero:"03",
+    categoria:"INTEGRAÇÃO | DADOS",
+    titulo:"CONSULTA CNPJ",
+    descricao:"A aplicação realiza consultas automatizadas de CNPJ e preenchimento dinâmico de informações, servindo como estudo prático de consumo de APIs, requisições assíncronas e estruturação de fluxo de dados.",
+    contexto:"Integração de dados | Comunicação com APIs",
+    exploracao:"HTML | CSS | JavaScript | XMLHTTPRequest | APIs REST",
+    aprendizado:"Fluxo assíncrono | Estruturação de requests | Tratamento de dados | CORS"
+  },
+  {
+    id:"biblioteca-acessivel",
+    numero:"04",
+    categoria:"ACESSIBILIDADE | HARDWARE | EXPERIÊNCIA",
+    titulo:"BIBLIOTECA ACESSÍVEL",
+    descricao:"A solução explora integração entre hardware e software para criação de uma experiência mais inclusiva dentro de ambientes educacionais.",
+    reconhecimento:"2º lugar no Ficthon Fecomércio/SENAC.",
+    contexto:"Acessibilidade | Inclusão | Experiência do usuário",
+    exploracao:"Arduino | Sensores | Estrutura lógica | Prototipagem"
+  }
+];
